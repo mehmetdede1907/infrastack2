3549deda0b7b469ca90d1fe831c66aa1569c588d
mehmet dedeler <99551919+mehmetdede1907@users.noreply.github.com>
2024-10-05 17:54:25 +0300

    finish

--- tools/main.py
+++ tools/main.py
@@ -61,4 +61,4 @@ async def get_results():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
